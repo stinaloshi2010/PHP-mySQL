020e8c19a2bc499d80dad03abad1951c62aa3a46
unknown <Student@digitalschool.local>
2025-10-01 20:28:29 +0200

    scope variables and arrays

---
+++ module_4/arrays.php
@@ -0,0 +1,41 @@
+<?php
+/*
+  $sports = ['Football', 'Basketball', 'Voleyball'];
+  echo $sports[1];
+
+  "<br>";
+
+  echo end($sports);
+
+  "<br>";
+  echo count($sports);
+
+
+
+$sports = array('Football','Basketball','voleyball');
+for($i = 0; $i < 3; $i++){
+    echo $sports[$i] , "\n";
+
+}
+
+$sports = array('Football','Basketball','Voleyball');
+ array_push($sports, "Golf");
+ var_dump($sports);
+
+
+$sports = array('Football','Basketball','Voleyball');
+ array_pop($sports);
+ var_dump($sports);
+
+
+ $sports = array('Football','Basketball','Voleyball');
+ array_unshift($sports, 'Tennis');
+ var_dump($sports);
+*/
+
+$sports = array('Football','Basketball','Voleyball');
+ array_shift($sports);
+ var_dump($sports);
+
+
+?>
---
+++ module_4/variables.php
@@ -0,0 +1,25 @@
+<?php
+/*
+$x = 5;
+
+function localvariables(){
+    $y = 10;
+    echo $y;
+}
+
+localvariables();
+echo" \n, $x";
+
+"<br>"
+*/
+
+$x = 5;
+$y = 7;
+function sum(){
+    global $x, $y;
+    $y = $x + $y;
+}
+sum();
+echo $y;
+
+?>
