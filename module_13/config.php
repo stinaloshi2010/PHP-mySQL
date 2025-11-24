a4057722cb1b08c1aa961a379f1940f8f5d3867c
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-11-24 20:23:21 +0100

    Movie sistem login form

---
+++ module_13/config.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+
+$user="root";
+$pass="";
+$server="localhost";
+$dbname="mms";
+
+
+try {
+    
+    $conn =new PDO("mysql:host=$server;dbname=$dbname",$user,$pass);
+
+
+} catch (PDOException $e) {
+    echo "error: " . $e->getMessage();
+}
+ ?>
+ 
